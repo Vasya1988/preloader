006c041c3a08231d2fca03f5df96fe5bdbfa73bb
Vasya1988 <myhappy1988@gmail.com>
2021-06-03 16:59:54 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const elements = {
-    test: document.querySelector('circle')
+    test: document.querySelector('circle'),
+    value: document.querySelector('#value'),
+    percentTitle: document.querySelector('.percent-title')
 }
 
-console.log(elements.test)
-console.dir(elements.test)
+const radius = elements.test.r.baseVal.value;
+const circumference = 2*Math.PI*radius;
+
+console.log(circumference);
+
+elements.test.style.strokeDasharray = `${circumference} ${circumference}`;
+elements.test.style.strokeDashoffset = circumference;
+
+// radius = (width / 2) - (strokeWidth * 2) - Находим радиус
+
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+
+    elements.test.style.strokeDashoffset = offset;
+}
+
+elements.value.addEventListener('keydown', (item) => {
+    if (item.keyCode === 13) {
+        item.preventDefault()
+        console.log(item.target.value);
+        console.log();
+
+        setProgress(item.target.value);
+        elements.percentTitle.innerText = `${item.target.value}%`
+
+    }
+})
