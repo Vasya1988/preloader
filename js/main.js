94c20afdbbadaaf62bf9b44d1855f772976ad4a1
Vasya1988 <myhappy1988@gmail.com>
2021-06-04 12:09:10 +0300

    created by as a module

--- js/main.js
+++ js/main.js
@@ -1,33 +1,74 @@
-const elements = {
-    test: document.querySelector('circle'),
-    value: document.querySelector('#value'),
-    percentTitle: document.querySelector('.percent-title')
-}
+export default function runPercent() {
+    
+    const markup = `
+    <div id="loading-round">
+    <div class="load-frame">
+        <svg>
+            <circle cx='70' cy='70' r='60'></circle>
+        </svg>
+        <span class="percent-title">0%</span>
+    </div>
 
-const radius = elements.test.r.baseVal.value;
-const circumference = 2*Math.PI*radius;
+    <form class="currentValue">
+        <input id='value' placeholder="Введите процент" type="text">
+    </form>
+    </div>
+    `;
 
-console.log(circumference);
+    // Разметка
+    function renderMarkup() {
+    return new Promise((resolve, reject) => {
+        document.querySelector('body').innerHTML = markup;
+        resolve();
+    })
+    };
 
-elements.test.style.strokeDasharray = `${circumference} ${circumference}`;
-elements.test.style.strokeDashoffset = circumference;
+    // radius = (width / 2) - (strokeWidth * 2) - Находим радиус
 
-// radius = (width / 2) - (strokeWidth * 2) - Находим радиус
+    // Изменение процента
+    function setPercent () {
+        return new Promise((resolve, reject) => {
+            const elements = {
+                test: document.querySelector('circle'),
+                value: document.querySelector('#value'),
+                percentTitle: document.querySelector('.percent-title')
+            }
 
-function setProgress(percent) {
-    const offset = circumference - percent / 100 * circumference;
+            const radius = elements.test.r.baseVal.value;
+            const circumference = 2*Math.PI*radius;
 
-    elements.test.style.strokeDashoffset = offset;
-}
+            elements.test.style.strokeDasharray = `${circumference} ${circumference}`;
+            elements.test.style.strokeDashoffset = circumference;
 
-elements.value.addEventListener('keydown', (item) => {
-    if (item.keyCode === 13) {
-        item.preventDefault()
-        console.log(item.target.value);
-        console.log();
+            elements.value.addEventListener('input', (item) => {
+                // if (item.keyCode === 13) {
+                //     item.preventDefault()
+                //     console.log(item.target.value);
+                //     console.log();
 
-        setProgress(item.target.value);
-        elements.percentTitle.innerText = `${item.target.value}%`
+                //     setProgress(item.target.value);
+                //     elements.percentTitle.innerText = `${item.target.value}%`;
+                // }
 
+                setProgress(item.target.value, circumference, elements.test);
+                    elements.percentTitle.innerText = `${item.target.value}%`;
+
+            })
+
+            resolve(circumference);
+        })
     }
-})
+
+    // Ф-я изменения процента
+    function setProgress(percent, circumference, element) {
+        const offset = circumference - percent / 100 * circumference;
+        element.style.strokeDashoffset = offset;
+    }
+
+    async function render() {
+        await renderMarkup();
+        await setPercent();
+    }
+    render()
+}
+runPercent()
